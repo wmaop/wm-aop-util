1592f08c5aa1ddcfc0e06ccba54084faae40bd4a
richard <richard@sysgen.co.uk>
2015-12-04 17:56:00 +0000

    IData to Yaml

---
+++ src/main/java/org/wmaop/util/yaml/YamlTransformer.java
@@ -0,0 +1,58 @@
+package org.wmaop.util.yaml;
+
+import java.io.InputStream;
+
+import org.wmaop.util.jexl.IDataJexlContext;
+
+import com.wm.data.IData;
+import com.wm.data.IDataCursor;
+
+public class YamlTransformer {
+
+	private final String INDENT = "   ";
+	
+	public String fromIData(IData idata) {
+		StringBuilder sb = new StringBuilder();
+		String indent = "";
+		parseIData(idata, sb, indent);
+		return sb.toString();
+	}
+
+	private void parseIData(IData idata, StringBuilder sb, String indent) {
+		IDataCursor idc = idata.getCursor();
+		while(idc.next()) {
+			String k = idc.getKey();
+			Object v = idc.getValue();
+			if (v instanceof IData[]) {
+				append(sb, indent, k, null);
+				IData[] idataArr = ((IData[]) v);
+				for (int i = 0; i < idataArr.length; i++) {
+					parseIData(idataArr[i], sb, indent + INDENT);
+				}
+			} else if (v instanceof IData) {
+				append(sb, indent, k, null);
+				parseIData((IData)v, sb, indent + INDENT);
+			} else {
+				append(sb, indent, k, v);
+			}
+		}
+	}
+	
+	private void append(StringBuilder sb, String indent, String k, Object v) {
+		sb.append(indent);
+		if (k.contains(":") || k.contains("*")) {
+			sb.append("\"").append(k).append("\"");
+		} else {
+			sb.append(k);
+		}
+		sb.append(": ");
+		if (v != null) {
+			if (v instanceof String) {
+				sb.append("\"").append(v.toString().trim()).append("\"");
+			} else {
+				sb.append(v.toString().trim());
+			}
+		}
+		sb.append("\r\n");
+	}
+}
--- src/test/java/org/wmaop/util/jexl/JexlExpressionFactoryTest.java
+++ src/test/java/org/wmaop/util/jexl/JexlExpressionFactoryTest.java
@@ -2,6 +2,8 @@ package org.wmaop.util.jexl;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.apache.commons.jexl2.Expression;
 import org.junit.Test;
 
@@ -9,6 +11,8 @@ import com.wm.data.IData;
 import com.wm.data.IDataCursor;
 import com.wm.data.IDataFactory;
 import com.wm.data.IDataUtil;
+import com.wm.util.coder.IDataXMLCoder;
+import com.wm.util.coder.InvalidDatatypeException;
 
 public class JexlExpressionFactoryTest {
 
@@ -44,5 +48,27 @@ public class JexlExpressionFactoryTest {
 		IDataCursor cursor = idata.getCursor();
 		IDataUtil.put(cursor, k, v);
 		cursor.destroy();
+	}
+	
+	
+	@Test
+	public void shouldCorrectlyAccessVariables() throws Exception {
+		IData idata = new IDataXMLCoder().decode(this.getClass().getClassLoader().getResourceAsStream("complex.xml"));
+		assertTrue(executeExpression("producer.prd_Id == \"2\"", idata));
+		assertTrue(executeExpression("producer.prd_GeneralPartyInfo.pty_Addr[0].adr_City == \"Napa\"", idata));
+		assertTrue(executeExpression("producer[0].bar != \"Napa\"", idata));
+
+		// Doesnt exist
+		assertFalse(executeExpression("producer.prd_GeneralPartyInfo.pty_Addr[1].adr_City == \"Napa\"", idata));
+		assertFalse(executeExpression("foo.bar == \"Napa\"", idata));
+		
+		// Bad array details
+		// Raise defect JEXL-183 as this blows rather than correctly returning true
+		//assertTrue(executeExpression("bar[0] != \"Napa\"", idata));
+	}
+	
+	private Boolean executeExpression(String expression, IData idata) {
+		Expression expr = JexlExpressionFactory.getEngine().createExpression(expression);
+		return (Boolean) expr.evaluate(new IDataJexlContext(idata));
 	}
 }
