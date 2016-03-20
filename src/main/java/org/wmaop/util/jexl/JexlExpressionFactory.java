85a5402c5f55a03ba39f6cf2420e172c4dbd12e4
richard <richard@sysgen.co.uk>
2016-03-20 09:06:52 +0000

    Tidy code

--- src/main/java/org/wmaop/util/jexl/JexlExpressionFactory.java
+++ src/main/java/org/wmaop/util/jexl/JexlExpressionFactory.java
@@ -8,6 +8,7 @@ import org.apache.commons.jexl2.JexlEngine;
 import org.wmaop.util.jexl.functions.ArraysFunction;
 
 public class JexlExpressionFactory {
+	
 	private static final JexlEngine jexlEngine = new JexlEngine();
 
 	static {
@@ -20,6 +21,8 @@ public class JexlExpressionFactory {
 
 		jexlEngine.setFunctions(funcs);
 	}
+
+	private JexlExpressionFactory() {}
 
 	public static Expression createExpression(String expr) {
 		return jexlEngine.createExpression(ExpressionProcessor.escapedToEncoded(expr));
